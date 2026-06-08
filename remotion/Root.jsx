d49ac05e0270cefe9976d56af3b80858cbf9e5fb
Rohan Santhosh Kumar <181558744+Rohan5commit@users.noreply.github.com>
2026-06-08 12:36:50 +0800

    Add Remotion Root composition

---
+++ remotion/Root.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { AbsoluteFill, useCurrentFrame, useVideoConfig, Sequence } from 'remotion';
+import script from './script.json' assert { type: 'json' };
+import { Intro } from './compositions/Intro.jsx';
+import { NewsSection } from './compositions/NewsSection.jsx';
+import { Explainer } from './compositions/Explainer.jsx';
+import { MarketSnapshot } from './compositions/MarketSnapshot.jsx';
+import { Outro } from './compositions/Outro.jsx';
+import { Background } from './components/Background.jsx';
+
+const TotalFrames = script.scenes.reduce((sum, s) => sum + s.durationSeconds * 30, 0);
+
+export const Root = () => {
+  return (
+    <AbsoluteFill>
+      <Composition
+        id="FinanceVideo"
+        component={FinanceVideo}
+        durationInFrames={TotalFrames}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+    </AbsoluteFill>
+  );
+};
+
+const FinanceVideo = ({ script: s }) => {
+  let frame = 0;
+  return (
+    <AbsoluteFill style={{ backgroundColor: '#0a0e1a' }}>
+      <Background />
+      {s.scenes.map((scene) => {
+        const durationInFrames = scene.durationSeconds * 30;
+        const from = frame;
+        frame += durationInFrames;
+        return (
+          <Sequence key={scene.id} from={from} durationInFrames={durationInFrames}>
+            <SceneComponent scene={scene} />
+          </Sequence>
+        );
+      })}
+    </AbsoluteFill>
+  );
+};
+
+const SceneComponent = ({ scene }) => {
+  switch (scene.type) {
+    case 'intro':
+      return <Intro scene={scene} />;
+    case 'news':
+      return <NewsSection scene={scene} />;
+    case 'explainer':
+      return <Explainer scene={scene} />;
+    case 'market':
+      return <MarketSnapshot scene={scene} />;
+    case 'outro':
+      return <Outro scene={scene} />;
+    default:
+      return <Intro scene={scene} />;
+  }
+};
